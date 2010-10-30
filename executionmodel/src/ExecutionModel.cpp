13e6795956078c4df537c1f1888ad634663b0164
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-29 23:20:17 -0400

    Adding embryonary execution model

---
+++ executionmodel/src/ExecutionModel.cpp
@@ -0,0 +1,12 @@
+/* 
+ * Look for all filter libraries
+ * Load all filter libraries
+ * Merge the available filters in all the filter libraries
+ */
+ExecutionModel::ExecutionModel() {}
+
+ExecutionModel::~ExecutionModel() {}
+
+FilterChain* ExecutionModel::loadFilterChain(std::string path) {
+	return 0;
+}
---
+++ executionmodel/src/ExecutionModel.h
@@ -0,0 +1,13 @@
+#ifndef EXECUTION_MODEL_H_32894712984712874127394871239847123987482317492
+#define EXECUTION_MODEL_H_32894712984712874127394871239847123987482317492
+
+class ExecutionModel {
+	public:
+		ExecutionModel();
+		~ExecutionModel();
+
+	public:
+		FilterChain* loadFilterChain(std::string path);
+};
+
+#endif
