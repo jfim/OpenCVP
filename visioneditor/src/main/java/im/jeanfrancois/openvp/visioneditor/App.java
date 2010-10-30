0e432a7037da3762c5a9accafc7e1615e0ef9147
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-30 00:08:35 -0400

    Use the system LaF

--- visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/App.java
+++ visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/App.java
@@ -4,16 +4,17 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import im.jeanfrancois.openvp.visioneditor.ui.MainWindow;
 
+import javax.swing.*;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-	    Injector injector = Guice.createInjector(new VisionEditorModule());
-	    MainWindow mainWindow = injector.getInstance(MainWindow.class);
-	    mainWindow.setVisible(true);
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+		Injector injector = Guice.createInjector(new VisionEditorModule());
+		MainWindow mainWindow = injector.getInstance(MainWindow.class);
+		mainWindow.setVisible(true);
+	}
 }
