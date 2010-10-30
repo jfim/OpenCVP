3ebf4cf004c5570f3b02f9b6ed7728fdd302c6e1
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-30 00:07:28 -0400

    Adding empty Guice module

---
+++ visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/VisionEditorModule.java
@@ -0,0 +1,14 @@
+package im.jeanfrancois.openvp.visioneditor;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * Guice module for the vision editor.
+ *
+ * @author jfim
+ */
+public class VisionEditorModule extends AbstractModule {
+	@Override
+	protected void configure() {
+	}
+}
