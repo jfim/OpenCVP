e8e08533f4ce8b13b16b1320cc081a30a80ce764
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-30 17:36:16 -0400

    Set Docking Frames theme to FlatTheme, looks much better

--- visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/VisionEditorAppLifecycleStarter.java
+++ visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/VisionEditorAppLifecycleStarter.java
@@ -1,5 +1,7 @@
 package im.jeanfrancois.openvp.visioneditor;
 
+import bibliothek.extension.gui.dock.theme.FlatTheme;
+import bibliothek.gui.dock.common.CControl;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import im.jeanfrancois.openvp.visioneditor.ui.GraphEditorPanel;
@@ -19,9 +21,10 @@ public class VisionEditorAppLifecycleStarter extends DockingLifecycle {
 	private GraphEditorPanel graphEditorPanel;
 
 	@Inject
-	public VisionEditorAppLifecycleStarter(VisionEditorMenuBar menuBar, GraphEditorPanel graphEditorPanel) {
+	public VisionEditorAppLifecycleStarter(VisionEditorMenuBar menuBar, GraphEditorPanel graphEditorPanel, CControl control) {
 		this.menuBar = menuBar;
 		this.graphEditorPanel = graphEditorPanel;
+		control.setTheme(new FlatTheme());
 	}
 
 	@Override
