419d9e31b85982bd21f83d37519a67e9bd7731fe
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-31 00:06:01 -0400

    Implemented all methods in FilterFactory

--- filterlibrary/src/FilterFactory.cpp
+++ filterlibrary/src/FilterFactory.cpp
@@ -4,14 +4,26 @@ FilterFactory::FilterFactory() {}
 FilterFactory::~FilterFactory() {}
 
 std::vector<std::string> FilterFactory::getRegisteredFilterClasses() {
-	std::vector<std::string> dummy;
-	return dummy;
+	// Create a copy of the list of registered filter classes
+	std::vector<std::string> registeredFilterClasses;
+
+	for(std::map<std::string, filterConstructorFunctor>::iterator iter = constructorFunctorMap.begin(); iter != constructorFunctorMap.end(); ++iter) {
+		registeredFilterClasses.push_back(iter->first);
+	}
+
+	return registeredFilterClasses;
 }
 
 Filter* FilterFactory::buildFilter(std::string className, std::string filterName) {
+	filterConstructorFunctor constructorFunctor = constructorFunctorMap[className];
+
+	if(constructorFunctor != 0) {
+		return constructorFunctor(filterName);
+	}
+
 	return 0;
 }
 
 void FilterFactory::registerFilterClass(std::string filterClassName, Filter* (*constructorFunctor)(std::string filterName)) {
-	constructorFunctorMap[filterClassName]=constructorFunctor;
+	constructorFunctorMap[filterClassName] = constructorFunctor;
 }
