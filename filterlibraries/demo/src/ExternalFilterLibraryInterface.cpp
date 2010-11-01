1eb847d3a782e516681abbfe487880833a74b068
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-31 20:47:29 -0400

    Added implementation of external filter library interface for the demo library

---
+++ filterlibraries/demo/src/ExternalFilterLibraryInterface.cpp
@@ -0,0 +1,31 @@
+#include <ExternalFilterLibraryInterface.h>
+#include <FilterFactory.h>
+
+#include <cstring>
+
+namespace {
+	const int SCRATCH_BUFFER_SIZE = 512;
+	char scratchBuffer[SCRATCH_BUFFER_SIZE];
+}
+
+BOOST_EXTENSION_EXPORT_DECL char* getFilterLibraryVendor() {
+	return "Jean-Francois Im";
+}
+
+BOOST_EXTENSION_EXPORT_DECL char* getFilterLibraryName() {
+	return "Demo Filters";
+}
+
+BOOST_EXTENSION_EXPORT_DECL char* getFilterLibraryVersion() {
+	return "1.0";
+}
+
+BOOST_EXTENSION_EXPORT_DECL int getRegisteredFilterClassCount() {
+	return FilterFactory::getInstance().getRegisteredFilterClasses().size();
+}
+
+BOOST_EXTENSION_EXPORT_DECL char* getRegisteredFilterClassName(int index) {
+	std::strncpy(scratchBuffer, FilterFactory::getInstance().getRegisteredFilterClasses().at(index).c_str(), SCRATCH_BUFFER_SIZE - 1);
+	scratchBuffer[SCRATCH_BUFFER_SIZE - 1] = '\0';
+	return scratchBuffer;
+}
