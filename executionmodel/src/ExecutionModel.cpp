e4b68d4d8ebee4b81232e3ce7ba02fa5b85a4ce9
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-31 21:26:29 -0400

    Added dependency on /site-config//boost for the execution model, added a dummy executable for testing the execution model

--- executionmodel/src/ExecutionModel.cpp
+++ executionmodel/src/ExecutionModel.cpp
@@ -1,11 +1,31 @@
 #include "ExecutionModel.h"
 
+#include <iostream>
+#include <boost/extension/shared_library.hpp>
+#include <boost/function.hpp>
+
+using namespace boost::extensions;
+using namespace boost;
+using namespace std;
+
 /* 
  * Look for all filter libraries
  * Load all filter libraries
  * Merge the available filters in all the filter libraries
  */
-ExecutionModel::ExecutionModel() {}
+ExecutionModel::ExecutionModel() {
+	shared_library lib("demo.dll");
+
+	if(!lib.open()) {
+		cerr << "Failed to open library!" << endl;
+	}
+
+	function<char* (void)> getVendor(lib.get<char*>("getFilterLibraryVendor"));
+
+	if(getVendor) {
+		cout << "Vendor: " << getVendor() << endl;
+	}
+}
 
 ExecutionModel::~ExecutionModel() {}
 
---
+++ executionmodel/src/main.cpp
@@ -0,0 +1,8 @@
+#include "ExecutionModel.h"
+
+// Dummy main for testing
+int main(int argc, char** argv) {
+	ExecutionModel executionModel;
+
+	return 0;
+}
