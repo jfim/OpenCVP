6db4cb08ced7dddf3a05d4f40ed6bead4f317fe7
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-11-04 00:08:52 -0400

    Added dummy property

--- visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/ui/PropertiesPanel.java
+++ visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/ui/PropertiesPanel.java
@@ -1,6 +1,8 @@
 package im.jeanfrancois.openvp.visioneditor.ui;
 
 import com.google.inject.Singleton;
+import com.l2fprod.common.propertysheet.AbstractProperty;
+import com.l2fprod.common.propertysheet.Property;
 import com.l2fprod.common.propertysheet.PropertySheetPanel;
 
 import javax.swing.*;
@@ -15,6 +17,51 @@ import java.awt.*;
 public class PropertiesPanel extends JPanel {
 	public PropertiesPanel() {
 		setLayout(new BorderLayout());
-		add(new PropertySheetPanel(), BorderLayout.CENTER);
+		final PropertySheetPanel propertySheetPanel = new PropertySheetPanel();
+
+		Property[] properties = new Property[1];
+		properties[0] = new AbstractProperty() {
+			@Override
+			public String getName() {
+				return "Dummy";
+			}
+
+			@Override
+			public String getDisplayName() {
+				return "Dummy";
+			}
+
+			@Override
+			public String getShortDescription() {
+				return "This is a dummy variable";
+			}
+
+			@Override
+			public Class getType() {
+				return String.class;
+			}
+
+			@Override
+			public boolean isEditable() {
+				return false;
+			}
+
+			@Override
+			public String getCategory() {
+				return "Blah";
+			}
+
+			@Override
+			public void readFromObject(Object o) {
+			}
+
+			@Override
+			public void writeToObject(Object o) {
+			}
+		};
+		propertySheetPanel.setProperties(properties);
+		propertySheetPanel.setDescriptionVisible(true);
+
+		add(propertySheetPanel, BorderLayout.CENTER);
 	}
 }
