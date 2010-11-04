8f43f2337f53c1baee9e7f715e7da3035510fa69
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-11-03 23:30:26 -0400

    Added outlook-style bar to the filter palette

--- visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/ui/FilterPalettePanel.java
+++ visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/ui/FilterPalettePanel.java
@@ -1,8 +1,13 @@
 package im.jeanfrancois.openvp.visioneditor.ui;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.l2fprod.common.swing.JOutlookBar;
+import im.jeanfrancois.openvp.nativebridge.ExecutionModel;
+import im.jeanfrancois.openvp.nativebridge.FilterLibrary;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Document me!
@@ -11,4 +16,17 @@ import javax.swing.*;
  */
 @Singleton
 public class FilterPalettePanel extends JPanel {
+	@Inject
+	public FilterPalettePanel(ExecutionModel executionModel) {
+		setLayout(new BorderLayout());
+
+		final JOutlookBar outlookBar = new JOutlookBar();
+
+		for (FilterLibrary filterLibrary : executionModel.getFilterLibraries()) {
+			String filterLibraryTitle = filterLibrary.getVendor() + " " + filterLibrary.getName() + " " + filterLibrary.getVersion();
+			outlookBar.add(filterLibraryTitle, new JTable());
+		}
+
+		add(outlookBar);
+	}
 }
