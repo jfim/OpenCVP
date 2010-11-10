88c3fbd56acadbc3301247830ea8339239bfbba0
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-11-10 00:20:17 -0500

    Added dummy statusbar

--- visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/ui/VisionEditorStatusBar.java
+++ visioneditor/src/main/java/im/jeanfrancois/openvp/visioneditor/ui/VisionEditorStatusBar.java
@@ -2,19 +2,31 @@ package im.jeanfrancois.openvp.visioneditor.ui;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import im.jeanfrancois.openvp.common.ui.JSparkline;
+import im.jeanfrancois.openvp.visioneditor.i18n.Localizer;
 import org.jdesktop.swingx.JXStatusBar;
+import org.jdesktop.swingx.plaf.basic.BasicStatusBarUI;
 
 import javax.swing.*;
 
 /**
- * Document me!
+ * Status bar for the vision editor
  *
  * @author jfim
  */
 @Singleton
 public class VisionEditorStatusBar extends JXStatusBar {
 	@Inject
-	public VisionEditorStatusBar() {
-		add(new JLabel(" "));
+	public VisionEditorStatusBar(Localizer localizer) {
+		// Do not auto-add separators
+		putClientProperty(BasicStatusBarUI.AUTO_ADD_SEPARATOR, false);
+
+		JSparkline sparkline = new JSparkline(50);
+		for(int i = 0; i < 50; ++i) {
+			sparkline.addDataElement((int) (Math.random() * 50));
+		}
+		add(new JLabel(" ", localizer.getLocalizedImageIcon("StatusBar.TimingPanel.icon"), JLabel.TRAILING));
+		add(sparkline);
+		add(new JLabel("Last timing 50ms"));
 	}
 }
