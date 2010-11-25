3a05b93d12885024e3924aa440a589053192f0ca
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-11-25 15:22:10 -0500

    Create the output directory if it doesn't exist

--- swig/runswig.bat
+++ swig/runswig.bat
@@ -1,1 +1,2 @@
+mkdir src\main\java\im\jeanfrancois\opencvp\nativeinterface\internal
 swig -c++ -java -Isrc/main/swig -Isrc/main/c++ -I../executionmodel/src -I../filterlibrary/src -package im.jeanfrancois.opencvp.nativeinterface.internal -outdir src/main/java/im/jeanfrancois/opencvp/nativeinterface/internal -o src/main/c++/OpenCVP_wrap.cxx -v OpenCVPNativeInterface.i
