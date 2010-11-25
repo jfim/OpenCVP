4bc40f571845bc9d5575dcffb5a70866eabcc35d
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-11-25 15:27:17 -0500

    Added the image source, a holder for the currently loaded movie or a camera

---
+++ visioneditor/src/main/java/im/jeanfrancois/opencvp/visioneditor/model/ImageSource.java
@@ -0,0 +1,20 @@
+package im.jeanfrancois.opencvp.visioneditor.model;
+
+import com.google.inject.Singleton;
+import im.jeanfrancois.opencvp.common.movie.JMFMovie;
+import im.jeanfrancois.opencvp.common.movie.Movie;
+
+import java.io.File;
+
+
+/**
+ * Holds the image source(movie, static image, camera, etc.) for the vision
+ * editor.
+ *
+ * @author jfim
+ */
+@Singleton
+public class ImageSource {
+	//private Movie movie = new JMFMovie(new File("C:/Users/jfim/Desktop/videos paintball/MOV01885.MPG"));
+
+}
