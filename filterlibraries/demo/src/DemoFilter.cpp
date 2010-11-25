11f841f47d61bbe4dd5d1a39295b7f8c42090468
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-11-25 13:38:45 -0500

    Wrote simple demo filter that sets the output image to all blue

--- filterlibraries/demo/src/DemoFilter.cpp
+++ filterlibraries/demo/src/DemoFilter.cpp
@@ -9,6 +9,23 @@ DemoFilter::DemoFilter(std::string filterName) :
 
 DemoFilter::~DemoFilter() {}
 
-void DemoFilter::execute() {}
+void DemoFilter::execute() {
+	outPort.ensureSizeAndType(inPort.getImage().size(), CV_8UC3);
+
+	// Fill the first channel with 255, the rest with 0
+	cv::Mat outputImage = outPort.getImage();
+	const int columns = outputImage.cols;
+	const int rows = outputImage.rows;
+	const size_t elementSize = outputImage.elemSize();
+	const size_t rowSize = outputImage.step;
+	unsigned char* ptr = outputImage.ptr();
+
+	for(int y = 0; y < rows; ++y)
+		for(int x = 0; x < columns; ++x) {
+			ptr[y * rowSize + x * elementSize + 0] = 255;
+			ptr[y * rowSize + x * elementSize + 1] =   0;
+			ptr[y * rowSize + x * elementSize + 2] =   0;
+		}
+}
 
 REGISTER_FILTER(DemoFilter);
